32319aa8963b85619deca3fc40da9c2472cdf9de
kitsmith29 <33060960+kitsmith29@users.noreply.github.com>
2021-07-23 21:21:32 +0100

    Extract playlist ids, save with names in df

--- user_playlists.py
+++ user_playlists.py
@@ -1,20 +1,20 @@
 from spotify_connect import SpotifyConnector
-from spotify_query import SpotifyQuery
+from spotify_query import SpotifyQueryHandler
 
 class UserPlaylists():
     
     def __init__(self, spotify_connection):
         self.sp_conn = spotify_connection
-        self.sp_query = SpotifyQuery()
+        self.sp_query = SpotifyQueryHandler()
 
-    def getUserPlaylistNames(self, username):    
+    def getUserPlaylistNamesIds(self, username):    
         playlist_df = self.sp_query.extractAllQueryResults(self.sp_conn, self.sp_conn.user_playlists, username)
-        playlist_names = playlist_df['name']
+        playlist_names = playlist_df[['name', 'id']]
         return(playlist_names)
-    
+          
 if __name__ == "__main__":
     connector = SpotifyConnector()
     sp = connector.connectToSpotify()
     up = UserPlaylists(sp)
-    playlist_names = up.getUserPlaylistNames('joekitkat')
-    print(playlist_names)
+    playlists = up.getUserPlaylistNamesIds('joekitkat')
+    print(playlists)
