2aa161b3cd9f24ca69da1cbd8b8733cdc1777896
kitsmith29 <33060960+kitsmith29@users.noreply.github.com>
2021-07-23 21:21:49 +0100

    Extract playlist tracks and artists

--- spotify_query.py
+++ spotify_query.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from spotify_connect import SpotifyConnector
 
-class SpotifyQuery():
+class SpotifyQueryHandler():
     
     def extractAllQueryResults(self, sp_conn, sp_func, params=None):
         results = sp_func(params)
@@ -11,3 +11,18 @@ class SpotifyQuery():
             total_results.extend(results['items'])
         total_df = pd.DataFrame(total_results)
         return(total_df)
+    
+    def extractTrackNames(self, track_df):
+        track_df = track_df['track'].apply(lambda x: x.get('name'))
+        return(track_df)
+        
+    def extractArtistNames(self, track_df):
+        artist_df = track_df['track'].apply(lambda x: x.get('artists'))
+        def retrieve_artists(x):
+            artist_list = []
+            num_of_artists = len(x)
+            for n in range(0, num_of_artists):
+                artist_list.append("%s" %(x[n].get('name')))
+            return(artist_list)
+        artist_df = artist_df.apply(lambda x: retrieve_artists(x))
+        return(artist_df)
