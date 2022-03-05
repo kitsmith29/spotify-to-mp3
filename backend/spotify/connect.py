b73f2251379a170df877b3a086be024d6f6e1a9a
kitsmith29 <33060960+kitsmith29@users.noreply.github.com>
2022-03-05 14:08:31 +0000

    Secrets stored in .env

--- backend/spotify/connect.py
+++ backend/spotify/connect.py
@@ -1,18 +1,19 @@
+from decouple import config
 from spotipy import oauth2, Spotify
+
 
 class SpotifyConnector(Spotify):
     def __init__(self):
         super().__init__(Spotify)
-        
-        self.client_id = 'c4f8cd3d6fd243e78390e38dc1cd6767'
-        self.client_secret = '86c216166e494bfaa82fb40b677f8aad'
-    
+
+        self.client_id = config["CLIENT_ID"]
+        self.client_secret = config["CLIENT_SECRET"]
+
     def connectToSpotify(self):
         auth_manager = oauth2.SpotifyClientCredentials(
-            client_id=self.client_id,
-            client_secret=self.client_secret
-            )
-        
+            client_id=self.client_id, client_secret=self.client_secret
+        )
+
         sp = Spotify(auth_manager=auth_manager)
-        
-        return(sp)
+
+        return sp
